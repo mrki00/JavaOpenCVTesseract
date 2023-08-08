3453da006835f58597c941ff2ad3065f7f7ab7e2
mrki00 <antonio.mrkoci@gmail.com>
2023-08-08 23:24:49 +0200

    working opencv and tesseract using static image, added hrv characters recognition and separated all info on scoreboard

--- src/main/java/OpencvTest.java
+++ src/main/java/OpencvTest.java
@@ -1,7 +1,8 @@
 public class OpencvTest {
 
         public static void main(String[] args) {
-            String[] rtspUrls = {"rtsp://localhost:8554/live4466/test"};
+            //String[] rtspUrls = {"rtsp://localhost:8554/live4466/test"};
+            String[] rtspUrls = {"rtsp://192.168.10.4:8554/live4466/test"};
             for (String rtspUrl : rtspUrls) {
                 RTSPThread thread = new RTSPThread(rtspUrl);
                 thread.start();
--- src/main/java/RTSPThread.java
+++ src/main/java/RTSPThread.java
@@ -2,100 +2,174 @@ import org.opencv.core.*;
 import org.opencv.highgui.HighGui;
 import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
-import org.opencv.videoio.VideoCapture;
-import org.opencv.videoio.Videoio;
 import net.sourceforge.tess4j.*;
 import java.awt.image.DataBufferByte;
 import java.awt.image.BufferedImage;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 class RTSPThread extends Thread {
     private String rtspUrl;
 
+    public static void searchPositions(String position, Mat imgRegion){
+        // Extract the position, size, and color for teamName1
+        String Position = section.get(position + "Position");
+        String Size = section.get(position + "Size");
+        String Color = section.get(position + "Color");
+
+        // Parse the position and size to get the x, y, width, and height
+        int x = Integer.parseInt(Position.substring(1, Position.indexOf('y')));
+        int y = Integer.parseInt(Position.substring(Position.indexOf('y') + 1));
+        int width1 = Integer.parseInt(Size.substring(0, Size.indexOf('x')));
+        int height1 = Integer.parseInt(Size.substring(Size.indexOf('x') + 1));
+
+        // Create a submat for the teamName1 section
+        Mat teamName1Section = imgRegion.submat(y, y + height1, x, x + width1);
+
+        // Convert the teamName1 section to a BufferedImage for OCR
+        int type1 = BufferedImage.TYPE_BYTE_GRAY;
+        BufferedImage teamName1Image = new BufferedImage(teamName1Section.width(), teamName1Section.height(), type1);
+        teamName1Section.get(0, 0, ((DataBufferByte) teamName1Image.getRaster().getDataBuffer()).getData());
+
+        // Perform OCR on the teamName1 section using Tesseract
+        ITesseract tesseract1 = new Tesseract();
+        tesseract1.setDatapath("/opt/homebrew/Cellar/tesseract/5.3.1_1/share/tessdata");
+        tesseract1.setLanguage("hrv");
+
+        try {
+            String ocrResult = tesseract1.doOCR(teamName1Image);
+            System.out.println("OCR result for teamName1: " + ocrResult);
+        } catch (TesseractException e) {
+            throw new RuntimeException(e);
+        }
+    }
     public RTSPThread(String rtspUrl) {
         this.rtspUrl = rtspUrl;
     }
-
+    static Map<String, String> section = new HashMap<>();
+    public List<String> positions = List.of("teamName1", "teamScore1","teamName2", "teamScore2", "time");
     @Override
     public void run() {
+        section.put("threshold", "0.65");
+        section.put("expectedGfxX", "460");
+        section.put("expectedGfxY", "46");
+        section.put("teamName1Position", "x12y2");
+        section.put("teamName1Size", "68x34");
+        section.put("teamName1Color", "black");
+        section.put("teamScore1Position", "x98y6");
+        section.put("teamScore1Size", "34x30");
+        section.put("teamScore1Color", "white");
+        section.put("teamName2Position", "x280y2");
+        section.put("teamName2Size", "68x34");
+        section.put("teamName2Color", "black");
+        section.put("teamScore2Position", "x228y6");
+        section.put("teamScore2Size", "34x30");
+        section.put("teamScore2Color", "white");
+        section.put("timePosition", "x156y26");
+        section.put("timeSize", "50x18");
+        section.put("timeColor", "white");
+
+
+
         System.out.println("in run");
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-        System.setProperty("jna.library.path", "/opt/homebrew/Cellar/tesseract/5.3.0_1/lib/");
-        VideoCapture capture = new VideoCapture();
-        capture.open(rtspUrl);
-        if (!capture.isOpened()) {
-            System.err.println("Failed to open RTSP stream: " + rtspUrl);
-            return;
-        }
-        capture.set(Videoio.CAP_PROP_FRAME_WIDTH, 1280);
-        capture.set(Videoio.CAP_PROP_FRAME_HEIGHT, 720);
+        System.setProperty("jna.library.path", "/opt/homebrew/Cellar/tesseract/5.3.1_1/lib/");
+
+        // VideoCapture capture = new VideoCapture();
+        // capture.open(rtspUrl);
+        // if (!capture.isOpened()) {
+        //     System.err.println("Failed to open RTSP stream: " + rtspUrl);
+        //     return;
+        // }
+        // capture.set(Videoio.CAP_PROP_FRAME_WIDTH, 1280);
+        // capture.set(Videoio.CAP_PROP_FRAME_HEIGHT, 720);
 
         Mat frame;
-        while (true) {
-            frame = new Mat();
-            capture.read(frame);
-            if (frame.empty()) {
-                System.err.println("Failed to read frame from RTSP stream: " + rtspUrl);
-                try {
-                    Thread.sleep(5000);
-                    capture.release();
-                    capture.open(rtspUrl);
-                    continue;
-                } catch (InterruptedException e) {
-                    capture.release();
-                }
+        // while (true) {
+        //     frame = new Mat();
+
+        //     capture.read(frame);
+        //     if (frame.empty()) {
+        //         System.err.println("Failed to read frame from RTSP stream: " + rtspUrl);
+        //         try {
+        //             Thread.sleep(5000);
+        //             capture.release();
+        //             capture.open(rtspUrl);
+        //             continue;
+        //         } catch (InterruptedException e) {
+        //             capture.release();
+        //         }
+        //     }
+        frame = Imgcodecs.imread("/Users/mrki/Downloads/testFrame.png");
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+
+        Mat exampleImage = Imgcodecs.imread("/Users/mrki/Downloads/score720.png");
+        Mat grayExampleImage = new Mat();
+        Imgproc.cvtColor(exampleImage, grayExampleImage, Imgproc.COLOR_BGR2GRAY);
+        int width = grayExampleImage.width();
+        int height = grayExampleImage.height();
+        Mat searchArea = frame.submat(Integer.parseInt(section.get("expectedGfxY")), Integer.parseInt(section.get("expectedGfxY")) + height, Integer.parseInt(section.get("expectedGfxX")), Integer.parseInt(section.get("expectedGfxX")) + width);
+        Mat graySearchArea = new Mat();
+        Imgproc.cvtColor(searchArea, graySearchArea, Imgproc.COLOR_BGR2GRAY);
+        Mat matchResult = new Mat();
+        Imgproc.matchTemplate(graySearchArea, grayExampleImage, matchResult, Imgproc.TM_CCOEFF_NORMED);
+        Core.MinMaxLocResult mmr = Core.minMaxLoc(matchResult);
+        Point matchLoc = mmr.maxLoc;
+        if (mmr.maxVal > Double.parseDouble(section.get("threshold"))) {
+            System.out.println(mmr.maxVal + " found in " + rtspUrl);
+            Point pt1 = new Point(matchLoc.x + Integer.parseInt(section.get("expectedGfxX")), matchLoc.y + Integer.parseInt(section.get("expectedGfxY")));
+            Point pt2 = new Point(matchLoc.x + Integer.parseInt(section.get("expectedGfxX")) + width, matchLoc.y + Integer.parseInt(section.get("expectedGfxY")) + height);
+            Imgproc.rectangle(frame, pt1, pt2, new Scalar(0, 0, 255), 2);
+
+            // Crop the search area to just the region where the image is found
+            Mat imageRegion = graySearchArea.submat((int) matchLoc.y, (int) (matchLoc.y + height), (int) matchLoc.x, (int) (matchLoc.x + width));
+
+            // Convert the cropped region to a BufferedImage for OCR
+            int type = BufferedImage.TYPE_BYTE_GRAY;
+            BufferedImage image = new BufferedImage(imageRegion.width(), imageRegion.height(), type);
+            imageRegion.get(0, 0, ((DataBufferByte) image.getRaster().getDataBuffer()).getData());
+
+            // Perform OCR on the cropped region using Tesseract
+            ITesseract tesseract = new Tesseract();
+            tesseract.setDatapath("/opt/homebrew/Cellar/tesseract/5.3.1_1/share/tessdata");
+            tesseract.setLanguage("hrv");
+
+            /*searchArea
+            for(int i = 0; i < 5; i++){
+                int x = Integer.parseInt(section.get("teamName1Position").substring(1, section.get("teamName1Position").indexOf('y')));
+
             }
+            Mat subsection = imageRegion.submat(y, y + height, x, x + width);
+*/
             try {
-                Thread.sleep(5000);
-            } catch (InterruptedException e) {
+                String ocrResult = tesseract.doOCR(image);
+                System.out.println("OCR result: " + ocrResult);
+            } catch (TesseractException e) {
                 throw new RuntimeException(e);
             }
-            Mat exampleImage = Imgcodecs.imread("/Users/mrki/Downloads/score720.png");
-            Mat grayExampleImage = new Mat();
-            Imgproc.cvtColor(exampleImage, grayExampleImage, Imgproc.COLOR_BGR2GRAY);
-            int x = 460;
-            int y = 46;
-            int width = grayExampleImage.width();
-            int height = grayExampleImage.height();
-            Mat searchArea = frame.submat(y, y + height, x, x + width);
-            Mat graySearchArea = new Mat();
-            Imgproc.cvtColor(searchArea, graySearchArea, Imgproc.COLOR_BGR2GRAY);
-            Mat matchResult = new Mat();
-            Imgproc.matchTemplate(graySearchArea, grayExampleImage, matchResult, Imgproc.TM_CCOEFF_NORMED);
-            double threshold = 0.65;
-            Core.MinMaxLocResult mmr = Core.minMaxLoc(matchResult);
-            Point matchLoc = mmr.maxLoc;
-            if (mmr.maxVal > threshold) {
-                System.out.println(mmr.maxVal + " found in " + rtspUrl);
-                Point pt1 = new Point(matchLoc.x + x, matchLoc.y + y);
-                Point pt2 = new Point(matchLoc.x + x + width, matchLoc.y + y + height);
-                Imgproc.rectangle(frame, pt1, pt2, new Scalar(0, 0, 255), 2);
 
-                // Crop the search area to just the region where the image is found
-                Mat imageRegion = graySearchArea.submat((int) matchLoc.y, (int) (matchLoc.y + height), (int) matchLoc.x, (int) (matchLoc.x + width));
-
-                // Convert the cropped region to a BufferedImage for OCR
-                int type = BufferedImage.TYPE_BYTE_GRAY;
-                BufferedImage image = new BufferedImage(imageRegion.width(), imageRegion.height(), type);
-                imageRegion.get(0, 0, ((DataBufferByte) image.getRaster().getDataBuffer()).getData());
-
-                // Perform OCR on the cropped region using Tesseract
-                ITesseract tesseract = new Tesseract();
-                tesseract.setDatapath("/opt/homebrew/Cellar/tesseract/5.3.0_1/share/tessdata");
-                try {
-                    String ocrResult = tesseract.doOCR(image);
-                    System.out.println("OCR result: " + ocrResult);
-                } catch (TesseractException e) {
-                    throw new RuntimeException(e);
-                }
-            }else{
-                System.out.println("No match found in " + rtspUrl);
+            for(int i = 0; i < positions.size(); i++){
+                searchPositions(positions.get(i), imageRegion);
             }
-        HighGui.imshow(rtspUrl, frame);
-        if (HighGui.waitKey(1) == 27) {
-            break;
+
+
+        }else{
+            System.out.println("No match found in " + rtspUrl);
         }
-    }
-        capture.release();
+        HighGui.imshow(rtspUrl, frame);
+
+        if (HighGui.waitKey(1) == 27) {
+            // break;
+        }
+        // }
+        // capture.release();
         HighGui.destroyAllWindows();
     }
 }
